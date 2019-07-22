94b7a90700388291e07eb6990554f8c88ef08d2b
Isaac Brown <isaac.brown@flexigroup.co.nz>
2019-07-22 19:42:30 +1200

    chore: linting

--- src/Muster.Core/Utility/Pagination/Metadata/PagedEnumerableMetadata.cs
+++ src/Muster.Core/Utility/Pagination/Metadata/PagedEnumerableMetadata.cs
@@ -2,10 +2,10 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-using System.ComponentModel;
-
 namespace Muster.Core.Utility.Pagination.Metadata
 {
+  using System.ComponentModel;
+
   /// <summary>
   /// Holds metadata for an <see cref="PagedEnumerable{TEntity}"/>.
   /// </summary>
