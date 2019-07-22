1afef425ae09cf50633a51d62a1b4c746356b73d
Isaac Brown <isaac.brown@flexigroup.co.nz>
2019-07-22 19:41:21 +1200

    feat(async-repository): add `Async` suffix to all methods

--- src/Muster.Core/Repositories/IAsyncRepository.cs
+++ src/Muster.Core/Repositories/IAsyncRepository.cs
@@ -20,21 +20,21 @@ namespace Muster.Core.Repositories
     /// </summary>
     /// <param name="entity">The <typeparamref name="TEntity"/> to add.</param>
     /// <returns>The <typeparamref name="TEntity"/> that was added.</returns>
-    Task<TEntity> Add(TEntity entity);
+    Task<TEntity> AddAsync(TEntity entity);
 
     /// <summary>
     /// Asynchronously checks for the existence of a single <typeparamref name="TEntity"/>.
     /// </summary>
     /// <param name="id">The identifier of the <typeparamref name="TEntity"/> to check the existence of.</param>
     /// <returns>true if there is a <typeparamref name="TEntity"/> with the given <paramref name="id"/>, false otherwise.</returns>
-    Task<bool> Exists(TKey id);
+    Task<bool> ExistsAsync(TKey id);
 
     /// <summary>
     /// Asynchronously get a single <typeparamref name="TEntity"/> by it's id.
     /// </summary>
     /// <param name="id">The identifier of the <typeparamref name="TEntity"/> to find.</param>
     /// <returns>The entity with the given <paramref name="id"/>, or null if it doesn't exist.</returns>
-    Task<TEntity> GetById(TKey id);
+    Task<TEntity> GetByIdAsync(TKey id);
 
     /// <summary>
     /// Asynchronously gets a paged collection of <typeparamref name="TEntity"/>s, skipping records
@@ -44,13 +44,13 @@ namespace Muster.Core.Repositories
     /// Default the is default of <typeparamref name="TKey"/> which will skip no records.</param>
     /// <param name="take">The number of records to take. Default is `100`.</param>
     /// <returns>A paged collection of <typeparamref name="TEntity"/>s.</returns>
-    Task<PagedEnumerable<TEntity>> GetPaged(TKey offset = default, int take = 100);
+    Task<PagedEnumerable<TEntity>> GetPagedAsync(TKey offset = default, int take = 100);
 
     /// <summary>
     /// Asynchronously updates a single <typeparamref name="TEntity"/> in the repository.
     /// </summary>
     /// <param name="entity">The <typeparamref name="TEntity"/> to update.</param>
     /// <returns>The <typeparamref name="TEntity"/> that was updated.</returns>
-    Task<TEntity> Update(TEntity entity);
+    Task<TEntity> UpdateAsync(TEntity entity);
   }
 }
